506663b288d9452f41060fa90666b096cd8b966f
Eric Dill <edill@bnl.gov>
2016-05-09 23:54:32 -0400

    Inital project setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import versioneer
+
+required = open('requirements.txt').read().split('\n')
+
+setup(
+    name='exceptional',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Report exceptions to the NSLS2 DAMA slack chat',
+    author='ericdill',
+    author_email='edill@bnl.gov',
+    url='https://github.com/nsls-ii/exceptional',
+    packages=['exceptional'],
+    install_requires=required,
+    long_description='See ' + 'https://github.com/nsls-ii/exceptional',
+    license='BSD 3-clause'
+)
